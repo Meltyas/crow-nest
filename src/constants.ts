ba851c520fa7f30888a83629b2c4ae949f92c1b1
Meltyas <34246177+Meltyas@users.noreply.github.com>
2025-07-11 21:42:25 +0200

    Add Admins module mirroring Patrols

--- src/constants.ts
+++ src/constants.ts
@@ -4,3 +4,4 @@ export const SETTING_LOG = "guard-stat-log";
 export const SETTING_MODIFIERS = "guard-modifiers";
 export const SETTING_RESOURCES = "guard-resources";
 export const SETTING_PATROLS = "patrols";
+export const SETTING_ADMINS = "admins";
