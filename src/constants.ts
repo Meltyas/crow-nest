762134ad7c5137f10dfacb7f55a703fd1503931a
Meltyas <34246177+Meltyas@users.noreply.github.com>
2025-07-11 17:41:15 +0200

    Add basic patrols component

--- src/constants.ts
+++ src/constants.ts
@@ -3,3 +3,4 @@ export const SETTING_STATS = "guard-stats";
 export const SETTING_LOG = "guard-stat-log";
 export const SETTING_MODIFIERS = "guard-modifiers";
 export const SETTING_RESOURCES = "guard-resources";
+export const SETTING_PATROLS = "patrols";
